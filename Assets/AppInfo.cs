80067d2fce11cd41a75297dd7d2ddbebfe9bddb4
y_okada <yasokada@gmail.com>
2015-09-10 19:17:29 +0900

    add AppInfo.cs to keep Application information such as version, name

---
+++ Assets/AppInfo.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace NS_appInfo // NS stands for NameSpace
+{
+	public static class AppInfo
+	{
+		public const string Version = "0.4";
+		public const string Name = "udpTimeGraph";
+	}
+}
