71397d156ff0492858b4a6391d4705bd4dba382b
y_okada <yasokada@gmail.com>
2015-09-11 21:38:24 +0900

    update Version string to "v0.5"

--- Assets/AppInfo.cs
+++ Assets/AppInfo.cs
@@ -12,7 +12,7 @@ namespace NS_appInfo // NS stands for NameSpace
 {
 	public static class AppInfo
 	{
-		public const string Version = "v0.4";
+		public const string Version = "v0.5";
 		public const string Name = "udpTimeGraph";
 	}
 }
