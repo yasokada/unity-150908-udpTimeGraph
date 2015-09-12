1b0efdc567bfb0dff44ce681e9e599b45b383cf5
y_okada <yasokada@gmail.com>
2015-09-12 15:42:56 +0900

    update version comment

--- Assets/AppInfo.cs
+++ Assets/AppInfo.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-/* v0.6 2015/09/12
+/* 
+ * v0.7 2015/09/12
+ *   - graph date is kept as <System.DateTime, float> so that x axis scale can be changed to weekly, etc.
+ * v0.6 2015/09/12
  *   - move several functions to MyPanelUtils.cs
  * v0.5 2015/09/11
  *   - display ymin and ymax on the left of the panel (graphScale)
@@ -13,7 +16,7 @@ namespace NS_appInfo // NS stands for NameSpace
 {
 	public static class AppInfo
 	{
-		public const string Version = "v0.6";
+		public const string Version = "v0.7";
 		public const string Name = "udpTimeGraph";
 	}
 }
