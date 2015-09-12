c30b56484b311084d94bb5be45b9358be6e19479
y_okada <yasokada@gmail.com>
2015-09-13 07:55:47 +0900

    update Test_weekly.cs

--- Assets/Test_weekly.cs
+++ Assets/Test_weekly.cs
@@ -5,16 +5,23 @@ public class Test_weekly : MonoBehaviour {
 
 	void Test_addWeeklyData() {
 
-		string [] dts = new string[]{ "09:30", "11:30", "13:30" };
-		float [] vals = new float[] { 0.5f, -0.9f, 0.9f };
+		string [] dts = new string[]{ 
+			"2015/09/07 12:30",
+			"2015/09/08 09:30", 
+			"2015/09/10 11:30", 
+			"2015/09/11 13:30" 
+		};
 
 		System.DateTime curDt;
 		float yval;
 		int idx = 0;
 
+		timeGraphScript.SetXType ((int)timeGraphScript.XType.Weekly);
+
 		foreach(var dt in dts) {
 			curDt = System.DateTime.Parse(dt);
-			yval = vals[idx];
+//			yval = vals[idx];
+			yval = Random.Range(-1.0f, 1.0f);
 			timeGraphScript.SetXYVal(curDt, yval);
 			idx++;
 		}
