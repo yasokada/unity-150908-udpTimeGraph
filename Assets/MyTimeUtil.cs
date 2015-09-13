91bbdcc41db0c45edebcb97cae3a370bacef0697
y_okada <yasokada@gmail.com>
2015-09-13 10:23:13 +0900

    fix getTimePosition_weekly() considering daysFrom [0,7)

--- Assets/MyTimeUtil.cs
+++ Assets/MyTimeUtil.cs
@@ -8,28 +8,33 @@ using System; // for TimeSpan
 namespace NS_MyTimeUtil
 {
 	public static class MyTimeUtil {
+
+		const int kDaysOfWeek = 7;
+
 		public static System.DateTime getSundayH00M00S00(System.DateTime dt) {
 			System.DateTime res = dt;
 			res -= new TimeSpan ((int)res.DayOfWeek, res.Hour, res.Minute, res.Second);
 			return res;
 		}
-		public static double getDaysFrom(System.DateTime fromDt, System.DateTime baseDt) {
-			return fromDt.Subtract (baseDt).TotalDays;
+		public static int getDaysFrom(System.DateTime fromDt, System.DateTime baseDt) {
+			return fromDt.Subtract (baseDt).Days;
 		}
 
 		public static float getTimePosition_weekly(System.DateTime dt)
 		{
-			System.DateTime sunday = MyTimeUtil.getSundayH00M00S00 (dt);
-			
-			int daysdiff = dt.Subtract (sunday).Days;
+			// find previous sunday based on Now
+			System.DateTime thisSunday = MyTimeUtil.getSundayH00M00S00 (System.DateTime.Now);
 
-			double daysFrom = getDaysFrom (dt, sunday);
-			Debug.Log ("daysfrom: " + daysFrom.ToString ("0"));
+			int daysFrom = getDaysFrom (dt, thisSunday);
+			Debug.Log ("daysfrom: " + daysFrom.ToString ());
 
+			if (daysFrom < 0 || daysFrom >= kDaysOfWeek) {
+				return -2.0f; // error. return less than -1.0f
+			}
 			
 			int hourMin_min = dt.Hour * 60 + dt.Minute;
 			float hourMinFraction = (float)hourMin_min / (24f * 60f); // 24 hours x 60 minutes
-			float ddhhmmFraction = (float)daysdiff + hourMinFraction;
+			float ddhhmmFraction = (float)daysFrom + hourMinFraction;
 			
 			//		Debug.Log ("target: " + dt.ToString ());
 			//		Debug.Log ("sunday: " + sunday.ToString ());
